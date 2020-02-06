2d3a18daf2bec3506863d6c550ab0c5d44c6f47c
Lauren E. Hostert <laurenehostert@gmail.com>
2020-02-06 17:12:37 -0600

    Find error causing incorrect taxa counts

--- tibble_section.R
+++ tibble_section.R
@@ -9,21 +9,39 @@ n_taxa_by_trait <- df %>%
   dplyr::summarise(count = n()) %>%
   filter(Family == 'Catostomidae')
 
+# Get summary of family richness
+sum_n_taxa_by_trait <- df %>% 
+  dplyr::group_by(Site_ID, Family) %>% 
+  dplyr::summarise(count = n())
+
+
 # Group counts by site ID (this will be a template to overwrite)
+# In this form it gives the total number of entries per sites which should be the same as # of species but it does not check for unique()  
 site_id_tibble <- df %>% 
   dplyr::group_by(Site_ID) %>% 
   dplyr::summarise(count = n())
 
 # Then mutate the counts to be those of n_taxa_by_family to
 # get a named vector of site IDs with the filtered family count
-#### LEH: When I run this outside of the function it seems that this does not retain the site ID information. 
-# Can we discuss whether or not this matter given the intended use of this function 
+
+### NEW
+site_id_tibble$count <- n_taxa_by_trait[match(site_id_tibble$Site_ID,n_taxa_by_trait$Site_ID),3]
+
 site_id_taxa_by_trait <- site_id_tibble %>%
-  dplyr::mutate(count = ifelse(Site_ID %in% n_taxa_by_trait$Site_ID, 
-                               n_taxa_by_trait$count,
-                               0)) %>%
+  replace_na(list(count.count = 0)) %>%
   dplyr::select(x = count, nm = Site_ID) %>%
   purrr::pmap(set_names) %>% 
   unlist
+return(site_id_taxa_by_trait)
 
+## TODO count int site_id_tibble becomes count.count then to replace NA it does not work. Fix this!
+
+### Original
+site_id_taxa_by_trait  <- site_id_tibble %>%
+  dplyr::mutate(count = ifelse(Site_ID %in% n_taxa_by_trait$Site_ID, 
+                               n_taxa_by_trait$count,
+                               0)) %>%
+  dplyr::select(x = count, nm = Site_ID) %>% 
+  purrr::pmap(set_names) %>% 
+  unlist
 return(site_id_taxa_by_trait)
