4de7683cc3dad3991bbca432af05841278c5cf3a
Lauren E. Hostert <laurenehostert@gmail.com>
2020-02-25 17:41:49 -0600

    Adjust file path for LDrake basin data

--- fish_matrix_binder.R
+++ fish_matrix_binder.R
@@ -4,10 +4,10 @@ network_prefix <- "//INHS-Bison"
 # network_prefix <- "/Volumes"
 
 crep <- read_csv(paste0(network_prefix,"/ResearchData/Groups/Kaskaskia_CREP/Analysis/Fish/Output/Fish_Metrics_CREP_2013-2019_final.csv"), na = "")
-basin <- read_csv(paste0(network_prefix,"/ResearchData/Groups/Kaskaskia_CREP/Analysis/Fish/Output/Fish_Metrics_IDNR_Basin_fit.csv"), na = "")
+basin <- read_csv(paste0(network_prefix,"/ResearchData/Groups/Kaskaskia_CREP/Analysis/Fish/Output/Fish_Metrics_Drake_1991-2007_final.csv"), na = "")
 
 
-fish_matrix <- bind_rows("crep_monitoring" = crep, "IDNR_basin_surveys" = basin, .id = "data_source")
+fish_matrix <- bind_rows("crep_monitoring" = crep, "IDNR_basin_surveys_LDrake" = basin, .id = "data_source")
 
 fish_matrix <- fish_matrix %>% select(2,1,3:112)
 
