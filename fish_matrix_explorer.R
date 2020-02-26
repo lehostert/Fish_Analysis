ddbe305bdb09721c21687c123b53aebe0483fb11
Lauren E. Hostert <laurenehostert@gmail.com>
2020-02-26 17:29:26 -0600

    Attempt to make loop for histogram generation

--- fish_matrix_explorer.R
+++ fish_matrix_explorer.R
@@ -1,7 +1,8 @@
 library(tidyverse)
+library(reshape2)
 
-# network_prefix <- "//INHS-Bison"
-network_prefix <- "/Volumes"
+network_prefix <- "//INHS-Bison"
+# network_prefix <- "/Volumes"
 
 fish <- read_csv(paste0(network_prefix,"/ResearchData/Groups/Kaskaskia_CREP/Analysis/Fish/Output/fish_matrix_full.csv"), na = c("", "."))
 names(fish) <- stringr::str_to_lower(names(fish))
@@ -20,6 +21,8 @@ ggplot2::ggplot(melt(fish), aes(x = value)) +
   labs(title=paste0("Kaskaskia Basin Fish Metrics"), y = "Count") +
   facet_wrap(~variable, scales = "free")
 
+tester <- melt(fish)
+
 #### Trial by Fire Work around ####
 # metric_list <- fish %>%
 #   select(-c(site_id, data_source)) %>% 
@@ -32,38 +35,119 @@ ggplot2::ggplot(melt(fish), aes(x = value)) +
 #             mmse_se = sd(i)/sqrt(n()),
 #             n_samples = n())
 #     }
-# 
-# by_metrics <- fish %>%
-#   select(-c(site_id, data_source))
-# 
-# sum_df <- by_metrics %>%
-#   summarise_all(list(~mean(.), ~sd(.), ~IQR(.), ~var(.), ~n_distinct(.)))
 
+by_metrics <- fish %>%
+  select(-c(site_id, data_source))
+
+sum_df <- tester %>%
+  group_by(variable) %>% 
+  summarise_all(list(~mean(.), ~sd(.), ~IQR(.), ~var(.), ~n_distinct(.))) %>% 
+  ungroup()
+
+
+
+
+fish_summary <- melt(sum_df)
+
+fish_summary$mean <- tester %>% 
+  
+  
 
 #### Loop Trial ####
-
 # metric_list <- fish %>%
 #   select(-c(site_id, data_source)) %>%
-#   names()
+#   names() %>% 
+#   list()
 # 
 # pdf(file="metric_plots.pdf")
 # 
 # plot_list = list()
 # 
 # for (i in metric_list) {
-#    p = ggplot2::ggplot(fish, aes(x = i, fill= data_source)) +
+#   x = fish[,i]
+#   plot_list[[i]] <- ggplot2::ggplot(data.frame(x), aes(x)) +
 #     geom_histogram() +
 #     theme(legend.position="top",plot.title = element_text(hjust=0.5) ,text = element_text(size=18, hjust=0.5)) +
-#     labs(title=paste0("Kaskaskia Basin Fish- ", i),x=paste0(i), y = "Count", fill = "Survey Type") +
-#     scale_color_manual(values=c("darkorchid1", "grey43", "#56B4E9")) +
-#     scale_fill_manual(values=c("darkorchid1", "grey43", "#56B4E9"))
-#    plot_list[[i]] = p
-# 
-#    print(plot_list[[i]])
+#     labs(title=paste0(i, " -Kaskaskia Basin Fish Metrics"),x=paste0(i), y = "Count")
+#   
+#   print(plot_list[[i]])
 # }
 # 
 # dev.off()
 
+
+metric_list <- fish %>%
+  select(-c(site_id, data_source)) 
+  # names() %>% 
+  # list()
+
+pdf(file="metric_plots.pdf")
+
+plot_list <- list()
+
+for (i in colnames(metric_list)) {
+  plot_list[[i]] <- ggplot2::ggplot(fish, aes(x = i)) +
+    geom_histogram()
+
+   print(plot_list[[i]])
+}
+
+dev.off()
+###
+metric_list = fish %>%
+  select(-c(site_id, data_source)) %>% 
+  colnames() %>% 
+  list()
+
+plot_list = list()
+
+for (i in seq_along(metric_list)) {
+  plot_list[[i]] = ggplot2::ggplot(fish, aes(x = metric_list[[i]])) +
+    geom_histogram()
+}
+
+pdf("plots.pdf")
+for (i in metric_list) {
+  print(plot_list[[i]])
+}
+dev.off()
+
+###
+a <- ggplot2::ggplot(fish, aes(x = individuals)) +
+  geom_histogram()
+
+print(a)
+
+
+for (i in seq_along(metric_list)){
+  print(metric_list[[i]])
+}
+
+#### Example ####
+# Plot separate ggplot figures in a loop.
+library(ggplot2)
+
+# Make list of variable names to loop over.
+var_list = combn(names(iris)[1:3], 2, simplify=FALSE)
+
+# Make plots.
+plot_list = list()
+for (i in 1:3) {
+  p = ggplot(iris, aes_string(x=var_list[[i]][1], y=var_list[[i]][2])) +
+    geom_point(size=3, aes(colour=Species))
+  plot_list[[i]] = p
+}
+
+# Another option: create pdf where each page is a separate plot.
+pdf("plots.pdf")
+for (i in 1:3) {
+  print(plot_list[[i]])
+}
+dev.off()
+
+
+
+#### Dragons ####
 # fish2 <- fish %>%
 #   select(-c((tidyselect::vars_select(names(fish), ends_with('ntax', ignore.case = TRUE)))))
 # 
