399a881ea248003fa7f3ef0cfb3b0020355c24fd
Lauren E. Hostert <laurenehostert@gmail.com>
2020-02-27 18:49:14 -0600

    Add site info table to matrix data and aggreagate data from same pugap_code

--- fish_matrix_binder.R
+++ fish_matrix_binder.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(reshape2)
 
 network_prefix <- "//INHS-Bison"
 # network_prefix <- "/Volumes"
@@ -6,9 +7,43 @@ network_prefix <- "//INHS-Bison"
 crep <- read_csv(paste0(network_prefix,"/ResearchData/Groups/Kaskaskia_CREP/Analysis/Fish/Output/Fish_Metrics_CREP_2013-2019_final.csv"), na = "")
 basin <- read_csv(paste0(network_prefix,"/ResearchData/Groups/Kaskaskia_CREP/Analysis/Fish/Output/Fish_Metrics_Drake_1991-2007_final.csv"), na = "")
 
+id_crep <-read_csv(paste0(network_prefix,"/ResearchData/Groups/Kaskaskia_CREP/Analysis/Fish/Output/id_table_CREP_2013-2019.csv"))
+id_basin <- read_csv(paste0(network_prefix,"/ResearchData/Groups/Kaskaskia_CREP/Analysis/Fish/Output/id_table_Drake_1991-2007.csv"))
+
 
 fish_matrix <- bind_rows("crep_monitoring" = crep, "IDNR_basin_surveys_LDrake" = basin, .id = "data_source")
+id_key <- bind_rows("crep_monitoring" = id_crep, "IDNR_basin_surveys_LDrake" = id_basin, .id = "data_source")
 
-fish_matrix <- fish_matrix %>% select(2,1,3:112)
+fish_matrix_full <- fish_matrix %>% 
+  full_join(id_key, by = c("Site_ID", "data_source")) %>% 
+  select(2,1,113,114,115,3:112)
 
+fish_matrix_full$data_source <- as.factor(fish_matrix_full$data_source)
+
+pugap_unique <- fish_matrix_full %>% 
+  select(pugap_code) %>% 
+  unique()
+
+pugap.source_unique <- fish_matrix_full %>% 
+  select(pugap_code,data_source) %>% 
+  unique()
+
+site_unique <- fish_matrix_full %>% 
+  select(Reach_Name) %>% 
+  unique()
+
+
+summary(pugap.source_unique)
+
+library(plyr)
+kasky.source_aggregate <- fish_matrix_full %>% 
+  select(-c(Site_ID, Reach_Name, Event_Date)) %>% 
+  plyr::ddply(.(data_source, pugap_code),colwise(mean))
+
+kasky_aggregate <- fish_matrix_full %>% 
+  select(-c(Site_ID, Reach_Name, Event_Date, data_source)) %>% 
+  plyr::ddply(.(pugap_code),colwise(mean))
+  
+
+write_csv(fish_matrix, paste0(network_prefix,"/ResearchData/Groups/Kaskaskia_CREP/Analysis/Fish/Output/fish_matrix_final.csv"), na = ".")
 write_csv(fish_matrix, paste0(network_prefix,"/ResearchData/Groups/Kaskaskia_CREP/Analysis/Fish/Output/fish_matrix_full.csv"), na = ".")
