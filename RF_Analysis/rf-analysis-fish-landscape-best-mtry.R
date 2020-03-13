6a8dd93bce74371e25fe05b2a96c5d5580e29421
Lauren E. Hostert <laurenehostert@gmail.com>
2020-03-13 16:15:06 -0500

    Fix broken for loop in best mtry script

--- RF_Analysis/rf-analysis-fish-landscape-best-mtry.R
+++ RF_Analysis/rf-analysis-fish-landscape-best-mtry.R
@@ -23,11 +23,13 @@ attach(metrics_envi.dat)
 set.seed(2020)
 for (i in 1:nrow(metrics_list))
 {
-    j <- metrics_list[i,2]  
+    j <- as.numeric(paste(metrics_list[i,2]))
+    
+    metric_name <- paste(metrics_list[i,1])
   
     metric <- get(paste(metrics_list[i,1]))
     
-    metric.rf <- randomForest(metric~link+dlink+c_order+dorder+wt_total_sqme+
+    fish.rf <- randomForest(metric~link+dlink+c_order+dorder+wt_total_sqme+
                               wt_gdd+wt_jul_mnx+wt_prec+
                               c_br50+c_br100+c_brg100+wt_br50+wt_br100+wt_brg100+
                               wt_br_carbonate+wt_br_sandstone+wt_br_shale+wt_rocky+wt_alluvium_fluvial+wt_coarse_moraine+wt_coarse+wt_colluvium+wt_dune+
@@ -39,46 +41,46 @@ for (i in 1:nrow(metrics_list))
                               w_crepcrp_percent+w_hel_percent, 
                               data = metrics_envi.dat, na.action = na.omit, ntree=5000,importance=T, mtry=j)
     
-    pdf(paste0(network_prefix,"/ResearchData/Groups/Kaskaskia_CREP/Analysis/Fish/Output/fish_RF_best_mtry_redo/fish_RF_VarImportance_",metric, ".pdf"), width = 9)
-    varImpPlot(metric.rf)
+    pdf(paste0(network_prefix,"/ResearchData/Groups/Kaskaskia_CREP/Analysis/Fish/Output/fish_RF_best_mtry_redo/fish_RF_VarImportance_",metric_name, ".pdf"), width = 9)
+    varImpPlot(fish.rf)
     dev.off()
     
-    imp_metric_rf <-importance(metric.rf)
-    imp_fish_RF <-data.frame(imp_fish_RF)
-    imp_fish_RF <- tibble::rownames_to_column(imp_fish_RF , "landscape_metric")
-    imp_fish_RF$fish_metric <- metric
-    imp_fish_RF$mtry <- j
+    imp_fish_rf <- importance(fish.rf)
+    imp_fish_rf <- data.frame(imp_fish_rf)
+    imp_fish_rf <- tibble::rownames_to_column(imp_fish_rf , "landscape_metric")
+    imp_fish_rf$fish_metric <- paste(metrics_list[i,1])
+    imp_fish_rf$mtry <- j
     
-    write.csv(imp_metric_rf, paste0(network_prefix,"/ResearchData/Groups/Kaskaskia_CREP/Analysis/Fish/Output/fish_RF_best_mtry_redo/fish_RF_VarImportance_",metric, ".csv"), row.names = T)
+    write.csv(imp_fish_rf, paste0(network_prefix,"/ResearchData/Groups/Kaskaskia_CREP/Analysis/Fish/Output/fish_RF_best_mtry_redo/fish_RF_VarImportance_",metric_name, ".csv"), row.names = T)
 }
 
 
 ##### Manual RF ####
-
-
-fish_metric <- "catontax"
-var_mtry <- 2
-fish_RF <- randomForest(metrics_envi.dat$catontax~link+dlink+c_order+dorder+wt_total_sqme+
-                          wt_gdd+wt_jul_mnx+wt_prec+
-                          c_br50+c_br100+c_brg100+wt_br50+wt_br100+wt_brg100+
-                          wt_br_carbonate+wt_br_sandstone+wt_br_shale+wt_rocky+wt_alluvium_fluvial+wt_coarse_moraine+wt_coarse+wt_colluvium+wt_dune+
-                          wt_fines+wt_lacustrine+wt_loess+wt_medium_moraine+wt_outwash+wt_peat_muck+wt_icecontact+
-                          w_darcyx+wt_darcyx+w_permx+wt_permx+
-                          r_open_wet+rt_grassland+w_forest_total+w_agriculture+w_grassland+w_urban+w_open_wet+w_wetland_total+wt_forest_total+wt_urban+wt_grassland+wt_agriculture+
-                          bigriver+damdwl+damdw+damupl+damup+missi+pond+pond_area+pondwl+pondwa+ponddw+pondupl+pondupa+pondup+
-                          sinuous+w_total_sqm+w_slope+wt_slope+gradient+
-                          w_crepcrp_percent+w_hel_percent,
-                        data = metrics_envi.dat, na.action = na.omit, ntree= 5000, mtry= var_mtry, importance= T)
-
-fish_RF
-R_value <- fish_RF$rsq[5000]
-
-imp_fish_RF <-importance(fish_RF)
-# habitat_list <- rownames(imp_fish_RF) 
-
-imp_fish_RF <-data.frame(imp_fish_RF)
-imp_fish_RF <- tibble::rownames_to_column(imp_fish_RF , "landscape_metric")
-imp_fish_RF$fish_metric <- fish_metric
+# 
+# 
+# fish_metric <- "catontax"
+# var_mtry <- 2
+# fish_RF <- randomForest(metrics_envi.dat$catontax~link+dlink+c_order+dorder+wt_total_sqme+
+#                           wt_gdd+wt_jul_mnx+wt_prec+
+#                           c_br50+c_br100+c_brg100+wt_br50+wt_br100+wt_brg100+
+#                           wt_br_carbonate+wt_br_sandstone+wt_br_shale+wt_rocky+wt_alluvium_fluvial+wt_coarse_moraine+wt_coarse+wt_colluvium+wt_dune+
+#                           wt_fines+wt_lacustrine+wt_loess+wt_medium_moraine+wt_outwash+wt_peat_muck+wt_icecontact+
+#                           w_darcyx+wt_darcyx+w_permx+wt_permx+
+#                           r_open_wet+rt_grassland+w_forest_total+w_agriculture+w_grassland+w_urban+w_open_wet+w_wetland_total+wt_forest_total+wt_urban+wt_grassland+wt_agriculture+
+#                           bigriver+damdwl+damdw+damupl+damup+missi+pond+pond_area+pondwl+pondwa+ponddw+pondupl+pondupa+pondup+
+#                           sinuous+w_total_sqm+w_slope+wt_slope+gradient+
+#                           w_crepcrp_percent+w_hel_percent,
+#                         data = metrics_envi.dat, na.action = na.omit, ntree= 5000, mtry= var_mtry, importance= T)
+# 
+# fish_RF
+# R_value <- fish_RF$rsq[5000]
+# 
+# imp_fish_RF <-importance(fish_RF)
+# # habitat_list <- rownames(imp_fish_RF) 
+# 
+# imp_fish_RF <-data.frame(imp_fish_RF)
+# imp_fish_RF <- tibble::rownames_to_column(imp_fish_RF , "landscape_metric")
+# imp_fish_RF$fish_metric <- fish_metric
 # Partial Dependancy Plots looping over variable to create for all variables. 
 # Remember y-values 
 # for (habitat_feature in seq_along(habitat_list)) {
@@ -88,25 +90,24 @@ imp_fish_RF$fish_metric <- fish_metric
 #   dev.off()
 # }
 
-#PLOT YOUR FORESTS IMPORTANT VARIABLES
-pdf(paste0(network_prefix,"/ResearchData/Groups/Kaskaskia_CREP/Analysis/Fish/Output/fish_RF_best_mtry/fish_RF_VarImportance_",fish_metric, ".pdf"), width = 9)
-varImpPlot(fish_RF)
-dev.off()
+# #PLOT YOUR FORESTS IMPORTANT VARIABLES
+# pdf(paste0(network_prefix,"/ResearchData/Groups/Kaskaskia_CREP/Analysis/Fish/Output/fish_RF_best_mtry/fish_RF_VarImportance_",fish_metric, ".pdf"), width = 9)
+# varImpPlot(fish_RF)
+# dev.off()
 
 
 write.csv(imp_fish_RF, paste0(network_prefix,"/ResearchData/Groups/Kaskaskia_CREP/Analysis/Fish/Output/fish_RF_best_mtry/fish_RF_VarImportance_",fish_metric, ".csv"), row.names = T)
 
 ######### Read  all CSV ##########
-# library(tidyverse)
-# 
-# rf_filenames <- list.files(path="//INHS-Bison/ResearchData/Groups/Kaskaskia_CREP/Analysis/Fish/Output/fish_RF_best_mtry", pattern= "*.csv")
-# rf_fullpath = file.path("//INHS-Bison/ResearchData/Groups/Kaskaskia_CREP/Analysis/Fish/Output/fish_RF_best_mtry", rf_filenames)
-# rf_fulldataset <- do.call("cbind",lapply(rf_fullpath, FUN = function(files){read.csv(files, stringsAsFactors = FALSE, na.strings = ".")}))
-# write.csv(rf_fulldataset, file= paste0("//INHS-Bison/ResearchData/Groups/Kaskaskia_CREP/Analysis/Fish/Output/fish_landscape_bestmtry_RF_VarImportance_20200311.csv"), na= "", row.names = F)
-# 
-# algntax <- read_csv(file = "//INHS-Bison/ResearchData/Groups/Kaskaskia_CREP/Analysis/Fish/Output/fish_RF_best_mtry/fish_RF_VarImportance_algntax.csv") %>% 
+
+rf_filenames <- list.files(path="//INHS-Bison/ResearchData/Groups/Kaskaskia_CREP/Analysis/Fish/Output/fish_RF_best_mtry_redo", pattern= "*.csv")
+rf_fullpath = file.path("//INHS-Bison/ResearchData/Groups/Kaskaskia_CREP/Analysis/Fish/Output/fish_RF_best_mtry_redo", rf_filenames)
+rf_fulldataset <- do.call("rbind",lapply(rf_fullpath, FUN = function(files){read.csv(files, stringsAsFactors = FALSE, na.strings = ".")}))
+write.csv(rf_fulldataset, file= paste0("//INHS-Bison/ResearchData/Groups/Kaskaskia_CREP/Analysis/Fish/Output/fish_landscape_bestmtry_RF_VarImportance_20200313.csv"), na= "", row.names = F)
+
+# algntax <- read_csv(file = "//INHS-Bison/ResearchData/Groups/Kaskaskia_CREP/Analysis/Fish/Output/fish_RF_best_mtry_redo/fish_RF_VarImportance_algntax.csv") %>%
 #   rename(hab_metric = 1)
-# algptax <- read_csv(file = "//INHS-Bison/ResearchData/Groups/Kaskaskia_CREP/Analysis/Fish/Output/fish_RF_best_mtry/fish_RF_VarImportance_algptax.csv") %>% 
+# algptax <- read_csv(file = "//INHS-Bison/ResearchData/Groups/Kaskaskia_CREP/Analysis/Fish/Output/fish_RF_best_mtry_redo/fish_RF_VarImportance_algptax.csv") %>%
 #   rename(hab_metric = 1)
 # 
 # combined <- algntax %>% full_join(algptax, by = "hab_metric")
